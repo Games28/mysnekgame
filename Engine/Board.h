d57a3244cc502cc6f1565bdfb0b1548ff97f72b1
Games28 <43530438+Games28@users.noreply.github.com>
2019-01-21 00:09:24 -0600

    Add project files.

---
+++ Engine/Board.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Graphics.h"
+#include "Location.h"
+#include <assert.h>
+
+class Board
+{
+public:
+	Board(Graphics& gfx);
+	void DrawCell(const Location& loc, Color c);
+	int GetGridWidth() const;
+	int GetGridHeight() const;
+private:
+	static constexpr int dimension = 20;
+	static constexpr int width = 25;
+	static constexpr int height = 25;
+	Graphics& gfx;
+};
